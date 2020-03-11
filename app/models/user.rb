fc75be7fb8d169c190fa2d5436da29a9573f2f17
Anneloes <anneloes9405@hotmail.com>
2020-03-11 20:15:13 +0000

    user specs now finally partially working, continuing on authenticate_with_credentials and the edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 3 }, confirmation: true
+  validates :password_confirmation, presence: true, length: { minimum: 3 }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,58 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'Validations' do
 
   user = User.create({ first_name: "Bob", last_name: "Bobberson" , email: "bobbobberson@bob.com", password: "hello", password_confirmation: "hello" })
 
+  describe 'Validations' do
+
     it 'saves correctly when all fields are entered correctly' do
-      user1 = user
-      user1.save
+      user.save
       expect(User.find_by(last_name: "Bobberson")).not_to be_nil
     end
 
     it 'should have password matching with password_confirmation' do
-      user2 = user
-      user2.password = "Hello"
-      user2.password_confirmation = "hello"
-      user2.save
-      expect(user2.password).not_to eq(user2.password_confirmation)
+      user.password = "Hello"
+      user.password_confirmation = "hello"
+      user.save
+      expect(user.password).not_to eq(user.password_confirmation)
     end
 
     it 'should not save correctly when password and password_confirmation are not entered' do
-      user3 = user
-      user3.password_confirmation = nil
-      user3.save
-      expect(user3).to_not be_valid
+      user.password_confirmation = nil
+      user.save
+      expect(user).to_not be_valid
     end
 
     it 'should not allow a new user to enter the same email address -> case-sensitivity' do
-      # p user4
-      user4 = user
-      user4.email = "BobBobberson@Bob.com"
-      user4.save
-      expect(User.find_by(id: 4)).to be_nil
+      user2 = user
+      user2.email = "BobBobberson@Bob.com"
+      user2.save
+      expect(User.find_by(id: 2)).to be_nil
     end
 
     it 'should not save correctly once email is not set and validated' do
-      user5 = user
-      user5.email = nil
-      user5.save
-      expect(User.find_by(id: 5)).to be_nil
+      user2 = user
+      user2.email = nil
+      user2.save
+      expect(User.find_by(id: 2)).to be_nil
     end
 
     it 'should not save correctly once first_name is not set and validated' do
-      user6 = user
-      user6.first_name = nil
-      user6.save
-      expect(User.find_by(id: 6)).to be_nil
+      user2 = user
+      user2.first_name = nil
+      user2.save
+      expect(User.find_by(id: 2)).to be_nil
     end
 
     it 'should not save correctly once last_name is not set and validated' do
-      user7 = user
-      user.last_name = nil
-      user.save
-      expect(User.find_by(id: 7)).to be_nil
+      user2 = user
+      user2.last_name = nil
+      user2.save
+      expect(User.find_by(id: 2)).to be_nil
     end
   end
+
+  describe 'Password requirements' do
+
+    it 'must have at least 3 characters' do
+      user2 = user
+      user2.password = "12"
+      user2.password_confirmation = "12"
+      user2.save
+      expect(User.find_by(id: 2)).to be_nil
+    end
+  end
+
+  describe ''
+
+
+
 end
