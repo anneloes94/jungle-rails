0555a1a055d9f6e2ebcfd39c4b61ca7521382d10
Anneloes <anneloes9405@hotmail.com>
2020-03-11 03:19:44 +0000

    multiple tests are still not working - due to bcrypt?

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,39 +2,37 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+
+    category = Category.new(name: "Apparel")
+    product = Product.new(id: 200, name: "Carniverous clothes hanger", price: 20, quantity: 2, category: category)
+
     it 'saves correctly when all fields are set' do
-      @category = Category.new(name: "Apparel")
-      @product = Product.new(id: 200, name: "Carniverous clothes hanger", price: 20, quantity: 2, category: @category)
-      @product.save
+      product.save 
       expect(Product.find_by(name: "Carniverous clothes hanger")).not_to be_nil
     end
 
     it 'does not save correctly once name is not set and validated' do
-      @category = Category.new(name: "Apparel")
-      @product = Product.new(id: 200, name: nil, price: 20, quantity: 2, category: @category)
-      @product.save
+      product.name = nil
+      product.save
       expect(Product.find_by(id: 200)).to be_nil
     end
 
     it 'does not save correctly once price is not set and validated' do
-      @category = Category.new(name: "Apparel")
-      @product = Product.new(id: 200, name: "Carniverous clothes hanger", price: nil, quantity: 2, category: @category)
-      @product.save
+      product.price = nil
+      product.save
       expect(Product.find_by(name: "Carniverous clothes hanger")).to be_nil
     end
 
     it 'does not save correctly once quantity is not set and validated' do
-      @category = Category.new(name: "Apparel")
-      @product = Product.new(id: 200, name: "Carniverous clothes hanger", price: 20, quantity: nil, category: @category)
-      @product.save
+      product.quantity = nil
+      product.save
       expect(Product.find_by(name: "Carniverous clothes hanger")).to be_nil
 
     end
 
     it 'does not save correctly once category is not set and validated' do
-      @category = Category.new(name: "Apparel")
-      @product = Product.new(id: 200, name: "Carniverous clothes hanger", price: 20, quantity: 2, category: nil)
-      @product.save
+      product.category = nil
+      product.save
       expect(Product.find_by(name: "Carniverous clothes hanger")).to be_nil
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,28 +2,57 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-#     It must be created with a password and password_confirmation fields
-#     These need to match so you should have an example for where they are not the same
-#     These are required when creating the model so you should also have an example for this
-# Emails must be unique (not case sensitive; for example, TEST@TEST.com should not be allowed if test@test.COM is in the database)
-# Email, first name, and last name should also be required
 
-    it 'does not save correctly when password and password_confirmation do not match' do
+  user = User.create({ first_name: "Bob", last_name: "Bobberson" , email: "bobbobberson@bob.com", password: "hello", password_confirmation: "hello" })
+
+    it 'saves correctly when all fields are entered correctly' do
+      user1 = user
+      user1.save
+      expect(User.find_by(last_name: "Bobberson")).not_to be_nil
     end
 
-    it 'does not save correctly when password and password_confirmation are not entered' do
+    it 'should have password matching with password_confirmation' do
+      user2 = user
+      user2.password = "Hello"
+      user2.password_confirmation = "hello"
+      user2.save
+      expect(user2.password).not_to eq(user2.password_confirmation)
     end
 
-    it 'should not allow case-sensitivity for emails' do
+    it 'should not save correctly when password and password_confirmation are not entered' do
+      user3 = user
+      user3.password_confirmation = nil
+      user3.save
+      expect(user3).to_not be_valid
     end
 
-    it 'does not save correctly once email is not set and validated' do
+    it 'should not allow a new user to enter the same email address -> case-sensitivity' do
+      # p user4
+      user4 = user
+      user4.email = "BobBobberson@Bob.com"
+      user4.save
+      expect(User.find_by(id: 4)).to be_nil
     end
 
-    it 'does not save correctly once first_name is not set and validated' do
+    it 'should not save correctly once email is not set and validated' do
+      user5 = user
+      user5.email = nil
+      user5.save
+      expect(User.find_by(id: 5)).to be_nil
     end
 
-    it 'does not save correctly once last_name is not set and validated' do
+    it 'should not save correctly once first_name is not set and validated' do
+      user6 = user
+      user6.first_name = nil
+      user6.save
+      expect(User.find_by(id: 6)).to be_nil
+    end
+
+    it 'should not save correctly once last_name is not set and validated' do
+      user7 = user
+      user.last_name = nil
+      user.save
+      expect(User.find_by(id: 7)).to be_nil
     end
   end
 end
